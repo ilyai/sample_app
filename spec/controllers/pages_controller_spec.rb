68fb318b3620f9228191f4f63804885b38f80207
Ilya I <osdev0@gmail.com>
2012-11-18 19:07:32 +0600

    Pages: added actions and tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+
+    it "should not have default content" do
+      get 'home'
+      response.should_not have_selector("h1", :content => "Pages#home")
+    end
   end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
       response.should be_success
     end
   end
